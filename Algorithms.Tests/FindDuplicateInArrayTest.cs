b531dc78e1ca2fef609863aca510ac454883e4bd
Alexander Batishchev <abatishchev@gmail.com>
2014-04-02 22:32:27 -0700

    Adding FindDuplicateInArray

---
+++ Algorithms.Tests/FindDuplicateInArrayTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.Tests
+{
+	[TestClass]
+	public class FindDuplicateInArrayTest
+	{
+		[TestMethod]
+		public void TestFindDuplicateInArray()
+		{
+			int[] arr = { 1, 3, 5, 2, 5 };
+
+			foreach (var test in new Func<int[], int>[]
+				{
+					FindDuplicateInArray.Do1,
+					FindDuplicateInArray.Do2,
+					FindDuplicateInArray.Do3,
+					FindDuplicateInArray.Do4,
+					FindDuplicateInArray.Do5,
+					FindDuplicateInArray.Do6,
+					//FindDuplicateInArray.Do7
+				})
+			{
+				int r = test(arr);
+				Assert.AreEqual(r, 5);
+			}
+		}
+	}
+}
