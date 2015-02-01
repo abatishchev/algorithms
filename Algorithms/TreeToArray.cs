95368bc432333c04ab6ef8eec61306071d68b207
Alexander Batishchev <abatishchev@gmail.com>
2015-01-31 21:44:31 -0800

    Adding TreeToArray

---
+++ Algorithms/TreeToArray.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms
+{
+	public class TreeToArray
+	{
+		public static TreeNode[] Do(TreeNode root, int count)
+		{
+			var arr = new TreeNode[count];
+			Do(arr, root, 0);
+			return arr;
+		}
+
+		private static void Do(IList<TreeNode> arr, TreeNode node, int level)
+		{
+			if (node == null) return;
+
+			if (level == 0)
+				arr[level] = node;
+
+			if (node.Left != null)
+			{
+				arr[2 * level + 1] = node.Left;
+				Do(arr, node.Left, level + 1);
+			}
+			if (node.Right != null)
+			{
+				arr[2 * level + 2] = node.Right;
+				Do(arr, node.Right, level + 1);
+			}
+		}
+	}
+}
