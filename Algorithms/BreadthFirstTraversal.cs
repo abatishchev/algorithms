1353266e4e5ff24e9a117137808f847aeb88122f
Alexander Batishchev <abatishchev@gmail.com>
2015-01-31 22:25:44 -0800

    Adding BreadthFirstTraversal

---
+++ Algorithms/BreadthFirstTraversal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+	public class BreadthFirstTraversal
+	{
+		public static void Do(TreeNode root, Action<TreeNode> visitor)
+		{
+			Queue<TreeNode> queue = new Queue<TreeNode>();
+			queue.Enqueue(root);
+			while (queue.Count > 0)
+			{
+				TreeNode current = queue.Dequeue();
+				if (current == null)
+					continue;
+				queue.Enqueue(current.Left);
+				queue.Enqueue(current.Right);
+
+				visitor(current);
+			}
+		}
+	}
+}
