6fa1c7e7ba8380e3c69c9617c1f14ad7060c9ca8
Alexander Batishchev <abatishchev@gmail.com>
2015-02-09 21:34:46 -0800

    Adding NthOrderStatistics

---
+++ Algorithms.Tests/NthOrderStatisticsTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class NthOrderStatisticsTest : NthOrderStatistics
+    {
+        [Theory]
+        [MemberData("GetData")]
+        public void Do(int[] arr, int expected)
+        {
+            int actual = Median(arr);
+
+            Assert.Equal(actual, expected);
+        }
+
+        public static IEnumerable<object[]> GetData
+        {
+            get
+            {
+                yield return new object[] { new[] { 9, 8, 7, 6, 5, 4, 3, 2, 1 }, 5 };
+            }
+        }
+    }
+}
