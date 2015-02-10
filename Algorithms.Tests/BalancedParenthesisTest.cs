eb0ec315b3edf29915f2af6aa367005ecc4e966c
Alexander Batishchev <abatishchev@gmail.com>
2015-02-09 21:35:26 -0800

    Adding BalancedParenthesis

---
+++ Algorithms.Tests/BalancedParenthesisTest.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace Algorithms.Tests
+{
+    public class BalancedParenthesisTest : BalancedParenthesis
+    {
+        [Theory]
+        [InlineData("{{{{{{{{{{}}}}}}}}}}", true)]
+        public void CheckParenthesis(string input, bool expected)
+        {
+            bool actual = Do1(input);
+
+            Assert.Equal(actual, expected);
+        }
+    }
+}
---
+++ Algorithms/BalancedParenthesis.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq;
+
+namespace Algorithms
+{
+	public class BalancedParenthesis
+	{
+		public bool Do1(string input)
+		{
+			return IsBalanced(input, "");
+		}
+
+		private static readonly char[] Opened = "([<{".ToCharArray();
+		private static readonly char[] Closed = ")]>}".ToCharArray();
+
+		private static bool IsOpened(char ch)
+		{
+			return Opened.Contains(ch);
+		}
+
+		private static bool IsClosed(char ch)
+		{
+			return Closed.Contains(ch);
+		}
+
+		private static bool IsMatching(char chOpen, char chClose)
+		{
+			return Array.IndexOf(Opened, chOpen) == Array.IndexOf(Closed, chClose);
+		}
+
+		private static bool IsBalanced(string input, string stack)
+		{
+			if (input.Length == 0)
+			{
+				return stack.Length == 0;
+			}
+			else
+			{
+				if (IsOpened(input[0]))
+				{
+					return IsBalanced(input.Substring(1), input[0] + stack);
+				}
+				else
+				{
+					if (IsClosed(input[0]))
+					{
+						return stack.Length != 0 && IsMatching(stack[0], input[0]) && IsBalanced(input.Substring(1), stack.Substring(1));
+					}
+					else
+					{
+						return IsBalanced(input.Substring(1), stack);
+					}
+				}
+			}
+		}
+	}
+}
