68e1e05092dac1f65b434a5301079e14e54d1654
Alexander Batishchev <abatishchev@gmail.com>
2015-02-20 20:25:17 -0800

    Adding TrinagleType

---
+++ Algorithms.Tests/TriangleTest.cs
@@ -0,0 +1,50 @@
+﻿using Xunit;
+
+namespace Algorithms.Tests
+{
+	public class TriangleTest : Triangle
+	{
+		[Theory]
+
+		// one or more is zero
+		[InlineData(0, 1, 2, TriangleType.Error)]
+		[InlineData(1, 0, 2, TriangleType.Error)]
+		[InlineData(1, 2, 0, TriangleType.Error)]
+		[InlineData(1, 0, 0, TriangleType.Error)]
+		[InlineData(0, 1, 0, TriangleType.Error)]
+		[InlineData(0, 0, 1, TriangleType.Error)]
+		[InlineData(0, 0, 0, TriangleType.Error)]
+
+		// one or more is negative
+		[InlineData(-1, 1, 2, TriangleType.Error)]
+		[InlineData(1, -1, 2, TriangleType.Error)]
+		[InlineData(1, 2, -1, TriangleType.Error)]
+		[InlineData(1, -1, -1, TriangleType.Error)]
+		[InlineData(-1, 1, -1, TriangleType.Error)]
+		[InlineData(-1, -1, 1, TriangleType.Error)]
+
+		// theorem violation
+		[InlineData(1, 1, 3, TriangleType.Error)]
+		[InlineData(1, 3, 1, TriangleType.Error)]
+		[InlineData(3, 1, 1, TriangleType.Error)]
+
+		// all sides are equal
+		[InlineData(1, 1, 1, TriangleType.Equilateral)]
+
+		// two sides are equal
+		[InlineData(2, 2, 3, TriangleType.Isosceles)]
+		[InlineData(3, 5, 3, TriangleType.Isosceles)]
+		[InlineData(5, 4, 4, TriangleType.Isosceles)]
+
+		// all sides are different
+		[InlineData(1, 2, 3, TriangleType.Scalene)]
+		[InlineData(1, 3, 2, TriangleType.Scalene)]
+		[InlineData(3, 2, 1, TriangleType.Scalene)]
+		public void Test(int a, int b, int c, TriangleType expected)
+		{
+			TriangleType actual = Do(a, b, c);
+
+			Assert.Equal(actual, expected);
+		}
+	}
+}
---
+++ Algorithms/Triangle.cs
@@ -0,0 +1,44 @@
+﻿using System.Runtime.Hosting;
+
+namespace Algorithms
+{
+	public enum TriangleType : byte
+	{
+		Scalene = 1, // неравносторонний
+		Isosceles = 2, // равнобедренный
+		Equilateral = 3, // равносторонний
+		Error = 4
+	}
+
+	public class Triangle
+	{
+		public TriangleType Do(int a, int b, int c)
+		{
+			// error case
+			if (a <= 0 || b <= 0 || c <= 0)
+			{
+				return TriangleType.Error;
+			}
+
+			// triangle inequality theorem
+			if (a + b < c || b + c < a || a + c < b)
+			{
+				return TriangleType.Error;
+			}
+
+			// all sides are equal
+			if (a == b && b == c)
+			{
+				return TriangleType.Equilateral;
+			}
+
+			// two sides are equal
+			if (a == b || a == c || b == c)
+			{
+				return TriangleType.Isosceles;
+			}
+
+			return TriangleType.Scalene;
+		}
+	}
+}
