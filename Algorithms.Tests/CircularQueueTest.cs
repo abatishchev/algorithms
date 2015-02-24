2374c919a49aa3c3877a92402842b0aafdbe2408
Alexander Batishchev <abatishchev@gmail.com>
2015-02-23 18:47:50 -0800

    Adding CircularQueue

---
+++ Algorithms.Tests/CircularQueueTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+	public class CircularQueueTest : CircularQueue
+	{
+		[Fact]
+		public void Enqueue_should_add_new_items_to_queue()
+		{
+			var queue = CircularQueue.Init(5);
+
+			queue.Enqueue(1);
+			queue.Enqueue(2);
+			queue.Enqueue(3);
+			queue.Enqueue(4);
+
+			AssertQueue(queue, 1, 2, 3, 4);
+		}
+
+		[Fact]
+		public void Enqueue_should_add_new_items_after_Dequeue()
+		{
+			var queue = CircularQueue.Init(5);
+
+			queue.Enqueue(1);
+			queue.Enqueue(2);
+			queue.Enqueue(3);
+			queue.Enqueue(4);
+			queue.Enqueue(5);
+
+			// queue reached full state
+
+			AssertQueue(queue, 1, 2, 3, 4, 5);
+
+			// queue reached empty state
+
+			queue.Enqueue(6);
+			queue.Enqueue(7);
+
+			AssertQueue(queue, 6, 7);
+		}
+
+		[Fact]
+		public void Enqueue_should_throw_exception_when_queue_is_full()
+		{
+			var queue = CircularQueue.Init(5);
+
+			queue.Enqueue(1);
+			queue.Enqueue(2);
+			queue.Enqueue(3);
+			queue.Enqueue(4);
+			queue.Enqueue(5);
+
+			// queue reached full state
+
+			Action action = () => queue.Enqueue(6);
+			Assert.Throws<InvalidOperationException>(action);
+
+			// queue is not corrupted by exception
+
+			AssertQueue(queue, 1, 2, 3, 4, 5);
+
+			// queue reached empty state
+
+			queue.Enqueue(7);
+			queue.Enqueue(8);
+
+			AssertQueue(queue, 7, 8);
+		}
+
+		[Fact]
+		public void Dequeue_should_throw_exception_when_queue_is_empty()
+		{
+			var queue = CircularQueue.Init(5);
+
+			Action action = () => queue.Dequeue();
+			Assert.Throws<InvalidOperationException>(action);
+
+			// queue is not corrupted by exception
+
+			queue.Enqueue(1);
+			queue.Enqueue(2);
+
+			AssertQueue(queue, 1, 2);
+		}
+
+		private static void AssertQueue(CircularQueue queue, params int[] expected)
+		{
+			foreach (int e in expected)
+			{
+				Assert.Equal(queue.Dequeue(), e);
+			}
+		}
+	}
+}
---
+++ Algorithms/CircularQueue.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Algorithms
+{
+	public class CircularQueue
+	{
+		private readonly int[] _items;
+
+		private int _head, _tail;
+
+		private readonly object _readLock = new object(), _writeLock = new object();
+
+		protected CircularQueue()
+		{
+		}
+
+		private CircularQueue(int capacity)
+		{
+			if (capacity <= 0)
+				throw new ArgumentOutOfRangeException("capacity", "Capacity can't be equal or less than 0");
+
+			_items = new int[capacity];
+
+			_head = -1;
+			_tail = -1;
+		}
+
+		public static CircularQueue Init(int capacity)
+		{
+			return new CircularQueue(capacity);
+		}
+
+		public void Enqueue(int item)
+		{
+			lock (_writeLock)
+			{
+				if ((_head == 0 && _tail == _items.Length - 1) || (_tail + 1 == _head))
+				{
+					throw new InvalidOperationException("Queue is full");
+				}
+
+				if (_tail == _items.Length - 1)
+				{
+					_tail = 0;
+				}
+				else
+				{
+					_tail++;
+				}
+
+				_items[_tail] = item;
+
+				if (_head == -1)
+				{
+					_head = 0;
+				}
+			}
+		}
+
+		public int Dequeue()
+		{
+			lock (_readLock)
+			{
+				if (_head == -1)
+				{
+					throw new InvalidOperationException("Queue is empty");
+				}
+
+				int item = _items[_head];
+
+				_items[_head] = -1;
+
+				if (_head == _tail)
+				{
+					_head = -1;
+					_tail = -1;
+				}
+				else if (_head == _items.Length - 1)
+				{
+					_head = 0;
+				}
+				else
+				{
+					_head++;
+				}
+				return item;
+			}
+		}
+	}
+}
