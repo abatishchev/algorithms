0796a1c5b8a8428d11bba9d924a67ed7a8c73450
Alexander Batishchev <abatishchev@gmail.com>
2015-02-23 19:18:29 -0800

    Adding BuildOrder

---
+++ Algorithms.Tests/BuildOrderTest.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+	public class BuildOrderTest : BuildOrder
+	{
+		[Theory]
+		[MemberData("GetData")]
+		public void BuildOrder(ICollection<Operation> ops, ICollection<OperationDependency> deps, IEnumerable<IEnumerable<Operation>> expected)
+		{
+			var actual = FindBuildOrder(ops, deps).ToArray();
+
+			actual.ShouldBeEquivalentTo(expected);
+		}
+
+		public static IEnumerable<object[]> GetData()
+		{
+			yield return new object[]
+			{
+				new[]
+				{
+					new Operation("A"),
+					new Operation("B"),
+					new Operation("C"),
+					new Operation("D"),
+
+					new Operation("M"),
+					new Operation("N"),
+
+					new Operation("X"),
+					new Operation("Y"),
+					new Operation("Z")
+				},
+				new[]
+				{
+					new OperationDependency("B", "A"),
+					new OperationDependency("C", "A"),
+					new OperationDependency("D", "C"),
+
+					new OperationDependency("Y", "X"),
+					new OperationDependency("Z", "Y")
+
+				},
+				new[]
+				{
+					new[]
+					{
+						new Operation("A"),
+						new Operation("B"),
+						new Operation("C"),
+						new Operation("D")
+					},
+					new[]
+					{
+						new Operation("M"),
+					},
+					new[]
+					{
+						new Operation("N"),
+					},
+					new[]
+					{
+						new Operation("Z"),
+						new Operation("Y"),
+						new Operation("X")
+					}
+				}
+			};
+		}
+	}
+}
---
+++ Algorithms/BuildOrder.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Algorithms
+{
+	public class BuildOrder
+	{
+		public IEnumerable<IEnumerable<Operation>> FindBuildOrder(ICollection<Operation> ops, ICollection<OperationDependency> deps)
+		{
+			var roots = FindRoots(ops, deps);
+			foreach (var root in roots)
+			{
+				var order = GetBuildOrder(root, ops.Except(roots).ToArray(), deps);
+				ops = ops.Except(order).ToArray();
+				yield return order;
+			}
+		}
+
+		private static ICollection<Operation> FindRoots(IEnumerable<Operation> ops, IEnumerable<OperationDependency> deps)
+		{
+			return ops.Where(o => IsRoot(o, deps)).ToArray();
+		}
+
+		private static bool IsRoot(Operation op, IEnumerable<OperationDependency> deps)
+		{
+			return deps.All(d => op != d.Child);
+		}
+
+		private static ICollection<Operation> GetBuildOrder(Operation root, ICollection<Operation> ops, ICollection<OperationDependency> deps)
+		{
+			var order = new List<Operation> { root };
+
+			GetBuildOrder(ops, deps, order);
+
+			return order;
+		}
+
+		private static void GetBuildOrder(ICollection<Operation> ops, ICollection<OperationDependency> deps, List<Operation> order)
+		{
+			var q = from op in ops
+					from dep in deps
+					where dep.Child == op && order.Contains(dep.Parent)
+					select op;
+
+			var arr = q.ToArray();
+			if (arr.Any())
+			{
+				order.AddRange(arr);
+
+				GetBuildOrder(ops.Except(order).ToArray(), deps, order);
+			}
+		}
+	}
+
+	[DebuggerDisplay("{Name}")]
+	public class Operation
+	{
+		public Operation(string name)
+		{
+			Name = name;
+		}
+
+		public string Name { get; private set; }
+
+		public override bool Equals(object obj)
+		{
+			var op = obj as Operation;
+			return !ReferenceEquals(op, null) && String.Equals(Name, op.Name);
+		}
+
+		public override int GetHashCode()
+		{
+			return Name != null ? Name.GetHashCode() : 0;
+		}
+
+		public static bool operator ==(Operation a, Operation b)
+		{
+			return !ReferenceEquals(a, null) &&
+				   !ReferenceEquals(b, null) &&
+				   a.Equals(b);
+		}
+
+		public static bool operator !=(Operation a, Operation b)
+		{
+			return !(a == b);
+		}
+	}
+
+	public class OperationDependency
+	{
+		public OperationDependency(string child, string parent)
+		{
+			Parent = new Operation(parent);
+			Child = new Operation(child);
+		}
+
+		public Operation Child { get; private set; }
+
+		public Operation Parent { get; private set; }
+	}
+}
