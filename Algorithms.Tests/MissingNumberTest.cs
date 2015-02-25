a8b1d73254155d64eff5ba19bf8392534f187c5d
Alexander Batishchev <abatishchev@gmail.com>
2015-02-25 11:03:12 -0800

    Adding MissingNumber

---
+++ Algorithms.Tests/MissingNumberTest.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+	public class MissingNumberTest : MissingNumber
+	{
+		[Theory]
+		[MemberData("GetData")]
+		public void MissingNumber1(ICollection<int> numbers, int expected)
+		{
+			int actual = FindMissingNumber1(numbers);
+
+			actual.Should().Be(expected);
+		}
+
+		[Theory]
+		[MemberData("GetData")]
+		public void MissingNumbe2(IList<int> numbers, int expected)
+		{
+			int actual = FindMissingNumber2(numbers);
+
+			actual.Should().Be(expected);
+		}
+
+		[Theory]
+		[MemberData("GetData")]
+		public void MissingNumbe3(ICollection<int> numbers, int expected)
+		{
+			int actual = FindMissingNumber3(numbers);
+
+			actual.Should().Be(expected);
+		}
+
+		public static IEnumerable<object[]> GetData()
+		{
+			yield return new object[]
+			{
+				new[] { 1, 5, 2, 3 }, 4
+			};
+		}
+	}
+}
---
+++ Algorithms/MissingNumber.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms
+{
+	public class MissingNumber
+	{
+		public int FindMissingNumber1(ICollection<int> input)
+		{
+			// naive way
+			var sorted = input.OrderBy(i => i).ToArray();
+			for (int i = 1; i < sorted.Length; i++)
+			{
+				int x = sorted[i - 1];
+				int y = sorted[i];
+				if (y - x != 1)
+					return x + 1;
+			}
+			return -1;
+		}
+
+		public int FindMissingNumber2(IList<int> input)
+		{
+			// using the sum formula total = n*(n+1)/2
+
+			int count = input.Count + 1; // existing number plus one missing
+			int expected = count * (count + 1) / 2;
+
+			int actual = input.Sum();
+			// which is basically equal to:
+			//foreach (int i in input)
+			//{
+			//	actual += i;
+			//}
+
+			return expected - actual;
+		}
+
+		public int FindMissingNumber3(ICollection<int> input)
+		{
+			// brute force using LINQ, less efficient but probably more readable
+
+			return Enumerable.Range(1, input.Count + 1).Except(input).Single();
+		}
+	}
+}
