efde06705d56a759447781318954d8f67333e298
Alexander Batishchev <abatishchev@gmail.com>
2015-02-25 12:20:17 -0800

    Adding MergeSortedSeq

---
+++ Algorithms.Tests/MergeSortedSeqTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+	public class MergeSortedSeqTest : MergeSortedSeq
+	{
+		[Theory]
+		[MemberData("GetData")]
+		public void MergeSortedSeq(IList<int> a, IList<int> b, IList<int> expected)
+		{
+			var actual = Merge(a, b).ToArray();
+
+			actual.ShouldBeEquivalentTo(expected);
+		}
+
+		public static IEnumerable<object[]> GetData()
+		{
+			yield return new object[]
+			{
+				new[] { 1, 2, 3 },
+				new[] { 4, 5, 6 },
+				new[] { 1, 2, 3, 4, 5, 6 }
+			};
+			yield return new object[]
+			{
+				new[] { 4, 5, 6 },
+				new[] { 1, 2, 3 },
+				new[] { 1, 2, 3, 4, 5, 6 }
+			};
+		}
+	}
+}
---
+++ Algorithms/MergeSortedSeq.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms
+{
+	public class MergeSortedSeq
+	{
+		public IEnumerable<int> Merge(IList<int> arr1, IList<int> arr2)
+		{
+			bool a = true, b = true;
+			int i = 0, j = 0;
+
+			while (a || b)
+			{
+				if (a && b)
+				{
+					if (arr1[i] <= arr2[j])
+					{
+						yield return arr1[i];
+						i++;
+						a = i < arr1.Count;
+					}
+					else
+					{
+						yield return arr2[j];
+						j++;
+						b = j < arr2.Count;
+					}
+				}
+				else if (a)
+				{
+					yield return arr1[i];
+					i++;
+					a = i < arr1.Count;
+				}
+				else if (b)
+				{
+					yield return arr2[j];
+					j++;
+					b = j < arr2.Count;
+				}
+			}
+		}
+	}
+}
