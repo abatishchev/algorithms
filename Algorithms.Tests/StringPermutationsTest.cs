414fdcb2920a2ce3673bce910d3329fa8291f881
Alexander Batishchev <abatishchev@gmail.com>
2015-02-24 20:52:05 -0800

    Adding StringPermutations

---
+++ Algorithms.Tests/StringPermutationsTest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+	public class StringPermutationsTest : StringPermutations
+	{
+		[Theory]
+		[MemberData("GetData")]
+		public void StringPermutations(string input, string[] expected)
+		{
+			var actual = GetStringPermutations(input).ToArray();
+
+			actual.ShouldBeEquivalentTo(expected);
+		}
+
+		public static IEnumerable<object[]> GetData()
+		{
+			yield return new object[]
+			{
+				"abc",
+				new[] { "abc", "acb", "bac", "bca", "cab", "cba" }
+			};
+		}
+	}
+}
---
+++ Algorithms/StringPermutations.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+	public class StringPermutations
+	{
+		public IEnumerable<string> GetStringPermutations(string input)
+		{
+			var output = new List<string>();
+			GetStringPermutations(input, String.Empty, output);
+			return output;
+		}
+
+		private static void GetStringPermutations(string input, string remaining, ICollection<string> output)
+		{
+			if (String.IsNullOrEmpty(input))
+			{
+				// recursion exit condition
+				output.Add(remaining);
+				return;
+			}
+
+			for (int i = 0; i < input.Length; i++)
+			{
+				string substring = String.Concat(input.Substring(0, i), input.Substring(i + 1));
+				GetStringPermutations(substring, remaining + input[i], output);
+			}
+		}
+	}
+}
