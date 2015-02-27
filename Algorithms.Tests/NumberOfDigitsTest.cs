3af0027e9d74545ec7e4b75e0f3212ca97479b1f
Alexander Batishchev <abatishchev@gmail.com>
2015-02-26 21:13:06 -0800

    Adding NumberOfDigits

---
+++ Algorithms.Tests/NumberOfDigitsTest.cs
@@ -0,0 +1,18 @@
+﻿using Xunit;
+
+namespace Algorithms.Tests
+{
+	public class NumberOfDigitsTest : NumberOfDigits
+	{
+		[Theory]
+		[InlineData(5)]
+		[InlineData(12)]
+		[InlineData(901)]
+		public void NumberOfDigits(int num)
+		{
+			var actual = GetNumberOfDigits(num);
+
+			Assert.Equal(actual, num.ToString().Length);
+		}
+	}
+}
---
+++ Algorithms/NumberOfDigits.cs
@@ -0,0 +1,15 @@
+﻿namespace Algorithms
+{
+	public class NumberOfDigits
+	{
+		public int GetNumberOfDigits(int num)
+		{
+			int i = 0;
+			do
+			{
+				i++;
+			} while ((num /= 10) > 0);
+			return i;
+		}
+	}
+}
