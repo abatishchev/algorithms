a8cc76b0a7ed6f2c6be34c9c5a5966d1b5192861
Alexander Batishchev <abatishchev@gmail.com>
2015-03-01 13:41:25 -0800

    Adding Wildcard

---
+++ Algorithms.Tests/WildcardTest.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+	public class WildcardTest : Wildcard
+	{
+		[Theory]
+		[MemberData("GetData")]
+		public void Wildcard(string pattern, string input, int expected)
+		{
+			int actual = GetNumberOfOccurrences(pattern, input);
+
+			actual.Should().Be(expected);
+		}
+
+		public static IEnumerable<object[]> GetData()
+		{
+			yield return new object[] { "a*b", "a0b", 1 };
+			yield return new object[] { "a*b*c", "a0b0c", 1 };
+			yield return new object[] { "a*a", "a0a0a", 3 };
+			yield return new object[] { "a*b", "ab", 1 };
+		}
+	}
+}
---
+++ Algorithms/Wildcard.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms
+{
+	public class Wildcard
+	{
+		public int GetNumberOfOccurrences(string pattern, string input)
+		{
+			var rules = GetRules(pattern).ToArray();
+
+			var substrings = GetSubstrings(input).ToArray();
+
+			var macthes = GetMacthes(substrings, rules).ToArray();
+
+			return macthes.Count();
+		}
+
+		private static IEnumerable<Rule> GetRules(string pattern)
+		{
+			return pattern.Select(CreateRule);
+		}
+
+		private static Rule CreateRule(char ch, int i)
+		{
+			switch (ch)
+			{
+				case '*':
+					return new WildcardRule(ch, i);
+				default:
+					return new Rule(ch, i);
+			}
+		}
+
+		private static IEnumerable<string> GetSubstrings(string input)
+		{
+			for (int i = 0; i < input.Length; i++)
+			{
+				for (int j = i + 1; j <= input.Length; j++)
+				{
+					yield return input.Substring(i, j - i);
+				}
+			}
+		}
+
+		private static IEnumerable<string> GetMacthes(string[] substrings, Rule[] rules)
+		{
+			return substrings.Where(substring =>
+									{
+										return rules.All(r => r.IsMatch(substring));
+									});
+		}
+	}
+
+	public class WildcardRule : Rule
+	{
+		public WildcardRule(char ch, int i)
+			: base(ch, i)
+		{
+		}
+
+		public override bool IsMatch(string substring)
+		{
+			return true;
+		}
+	}
+
+	public class Rule
+	{
+		public Rule(char ch, int i)
+		{
+			Character = ch;
+			Position = i;
+		}
+
+		public char Character { get; private set; }
+
+		public int Position { get; private set; }
+
+		public virtual bool IsMatch(string substring)
+		{
+			if (String.IsNullOrEmpty(substring))
+				throw new ArgumentNullException("substring");
+
+			if (substring.Length < Position + 1)
+				return false;
+
+			return substring[Position] == Character;
+		}
+	}
+}
