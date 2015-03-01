8edf03d173789a3214673e9fe85e17fc85e9807e
Alexander Batishchev <abatishchev@gmail.com>
2015-03-01 13:41:34 -0800

    Adding Horsepool

---
+++ Algorithms.Tests/HorsepoolTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+	public class HorsepoolTest : Horsepool
+	{
+		[Theory]
+		[MemberData("GetData")]
+		public void Horsepool(string input, string searchTerm, int expected)
+		{
+			int actual = IndexOf(input, searchTerm);
+
+			actual.Should().Be(expected);
+		}
+
+		public static IEnumerable<object[]> GetData()
+		{
+			yield return new object[] { "abc", "ab", 0 };
+			yield return new object[] { "cab", "ab", 1 };
+		}
+	}
+}
---
+++ Algorithms/Horsepool.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms
+{
+	public class Horsepool
+	{
+		public int IndexOf(string input, string searchTerm)
+		{
+			var shiftTable = CreateShiftTable(searchTerm, input);
+
+			int i = searchTerm.Length - 1;
+			while (i <= input.Length - 1)
+			{
+				int k = 0;
+				while (k <= searchTerm.Length - 1 && searchTerm[searchTerm.Length - 1 - k] == input[i - k])
+				{
+					k++;
+				}
+
+				if (k == searchTerm.Length)
+				{
+					return i - searchTerm.Length + 1;
+				}
+
+				i = i + shiftTable[input[i]];
+			}
+			return -1;
+		}
+
+		private static IDictionary<char, int> CreateShiftTable(string input, string searchTerm)
+		{
+			var shiftTable = new Dictionary<char, int>();
+
+			foreach (char t in searchTerm)
+			{
+				if (shiftTable.ContainsKey(t))
+				{
+					continue;
+				}
+
+				if (!input.Contains(t.ToString()))
+				{
+					shiftTable.Add(t, input.Length);
+				}
+				else
+				{
+					for (int j = input.Length - 1; j >= 0; j--)
+					{
+						if (input[j] == t)
+						{
+							shiftTable.Add(t, input.Length - j - 1);
+							break;
+						}
+					}
+				}
+			}
+
+			return shiftTable;
+		}
+	}
+}
