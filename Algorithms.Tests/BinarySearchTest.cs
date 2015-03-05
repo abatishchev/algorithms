a1e11a0d7e5f2172eb6a9167b258a820728f2d92
Alexander Batishchev <abatishchev@gmail.com>
2015-03-05 13:26:17 -0800

    Adding BinarySearch

---
+++ Algorithms.Tests/BinarySearchTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+	public class BinarySearchTest : BinarySearch
+	{
+		[Theory]
+		[MemberData("GetData")]
+		public void BinarySearch(IList<int> input, int key, int expected)
+		{
+			var actual = Find(input, key);
+
+			actual.Should().Be(expected);
+		}
+
+		public static IEnumerable<object[]> GetData()
+		{
+			var arr = new[] { 1, 2, 3, 4, 5, 6 };
+
+			yield return new object[] { arr, 6, 5 };
+			yield return new object[] { arr, 1, 0 };
+			yield return new object[] { arr, 3, 2 };
+			yield return new object[] { arr, 0, -1 };
+			yield return new object[] { arr, 7, -1 };
+
+			var random = new Random();
+			arr = Enumerable.Range(random.Next(25), random.Next(25)).ToArray();
+			int key = arr[arr.Length / 2];
+			yield return new object[] { arr, key, Array.IndexOf(arr, key) };
+		}
+	}
+}
---
+++ Algorithms/BinarySearch.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms
+{
+	public class BinarySearch
+	{
+		public int Find(IList<int> input, int key)
+		{
+			if (input.Count == 0)
+				return -1;
+			if (key < input[0])
+				return -1;
+			if (key > input[input.Count - 1])
+				return -1;
+
+			return Find(input, key, 0, input.Count - 1);
+		}
+
+		private static int Find(IList<int> input, int key, int lo, int hi)
+		{
+			int mid = (lo + hi) / 2;
+
+			if (key < input[mid])
+				return Find(input, key, lo, mid - 1);
+			if (key > input[mid])
+				return Find(input, key, mid + 1, hi);
+
+			return mid;
+		}
+	}
+}
