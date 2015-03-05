6278b42b53968109b9e0d087952ec05c911867ac
Alexander Batishchev <abatishchev@gmail.com>
2015-03-04 16:22:31 -0800

    Adding PrimeNumbers

---
+++ Algorithms.Tests/PrimeNumbersTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+	public class PrimeNumbersTest : PrimeNumbers
+	{
+		[Theory]
+		[MemberData("GetData")]
+		public void PrimeNumbers(int max, ICollection<int> expected)
+		{
+			var actual = GetPrimeNumbers(17);
+
+			actual.ShouldBeEquivalentTo(expected);
+		}
+
+		public static IEnumerable<object[]> GetData()
+		{
+			yield return new object[] { 60, new[] { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59 } };
+		}
+	}
+}
---
+++ Algorithms/PrimeNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+	public class PrimeNumbers
+	{
+		public ICollection<int> GetPrimeNumbers(int max)
+		{
+			var primes = new List<int> { 2 };
+
+			int i = 3;
+			while (primes.Count < max)
+			{
+				var sqrt = (int)Math.Sqrt(i);
+
+				bool isPrime = true;
+				for (int j = 0; primes[j] <= sqrt; j++)
+				{
+					if (i % primes[j] == 0)
+					{
+						isPrime = false;
+						break;
+					}
+				}
+				if (isPrime)
+				{
+					primes.Add(i);
+				}
+
+				i += 2;
+			}
+
+			return primes;
+		}
+	}
+}
