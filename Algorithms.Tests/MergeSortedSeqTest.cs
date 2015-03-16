ca4bbe558514b5922fcb3667b0f281df52d8c955
Alexander Batishchev <abatishchev@gmail.com>
2015-03-16 14:14:05 -0700

    Adding Merge3

--- Algorithms.Tests/MergeSortedSeqTest.cs
+++ Algorithms.Tests/MergeSortedSeqTest.cs
@@ -8,15 +8,24 @@ namespace Algorithms.Tests
 	public class MergeSortedSeqTest : MergeSortedSeq
 	{
 		[Theory]
-		[MemberData("GetData")]
-		public void MergeSortedSeq(IList<int> a, IList<int> b, IList<int> expected)
+		[MemberData("GetData2")]
+		public void MergeSortedSeq2(IList<int> a, IList<int> b, IList<int> expected)
 		{
 			var actual = Merge(a, b).ToArray();
 
 			actual.ShouldBeEquivalentTo(expected);
 		}
 
-		public static IEnumerable<object[]> GetData()
+		[Theory]
+		[MemberData("GetData3")]
+		public void MergeSortedSeq3(IList<int> a, IList<int> b, IList<int> c, IList<int> expected)
+		{
+			var actual = Merge(a, b, c).ToArray();
+
+			actual.ShouldBeEquivalentTo(expected);
+		}
+
+		public static IEnumerable<object[]> GetData2()
 		{
 			yield return new object[]
 			{
@@ -29,6 +38,17 @@ namespace Algorithms.Tests
 				new[] { 4, 5, 6 },
 				new[] { 1, 2, 3 },
 				new[] { 1, 2, 3, 4, 5, 6 }
+			};
+		}
+
+		public static IEnumerable<object[]> GetData3()
+		{
+			yield return new object[]
+			{
+				new[] { 1, 2 },
+				new[] { 4, 5 },
+				new[] { 3, 4 },
+				new[] { 1, 2, 3, 4, 4, 5 }
 			};
 		}
 	}
--- Algorithms/MergeSortedSeq.cs
+++ Algorithms/MergeSortedSeq.cs
@@ -15,30 +15,84 @@ namespace Algorithms
 				{
 					if (arr1[i] <= arr2[j])
 					{
-						yield return arr1[i];
-						i++;
-						a = i < arr1.Count;
+						yield return Advance(arr1, ref i, ref a);
 					}
 					else
 					{
-						yield return arr2[j];
-						j++;
-						b = j < arr2.Count;
+						yield return Advance(arr2, ref j, ref b);
 					}
 				}
 				else if (a)
 				{
-					yield return arr1[i];
-					i++;
-					a = i < arr1.Count;
+					yield return Advance(arr1, ref i, ref a);
 				}
 				else if (b)
 				{
-					yield return arr2[j];
-					j++;
-					b = j < arr2.Count;
+					yield return Advance(arr2, ref j, ref b);
 				}
 			}
+		}
+
+		public static IEnumerable<int> Merge(IList<int> arr1, IList<int> arr2, IList<int> arr3)
+		{
+			bool a = true, b = true, c = true;
+			int i = 0, j = 0, k = 0;
+
+			while (a || b || c)
+			{
+				if (a && b)
+				{
+					if (arr1[i] <= arr2[j])
+					{
+						yield return Advance(arr1, ref i, ref a);
+					}
+					else if (arr2[j] <= arr1[i])
+					{
+						yield return Advance(arr2, ref j, ref b);
+					}
+				}
+				if (a && c)
+				{
+					if (arr1[i] <= arr3[k])
+					{
+						yield return Advance(arr1, ref i, ref a);
+					}
+					else if (arr3[k] <= arr1[i])
+					{
+						yield return Advance(arr3, ref k, ref c);
+					}
+				}
+				if (b && c)
+				{
+					if (arr2[j] <= arr3[k])
+					{
+						yield return Advance(arr2, ref j, ref b);
+					}
+					else if (arr3[k] <= arr2[j])
+					{
+						yield return Advance(arr3, ref k, ref c);
+					}
+				}
+				if (a)
+				{
+					yield return Advance(arr1, ref i, ref a);
+				}
+				if (b)
+				{
+					yield return Advance(arr2, ref j, ref b);
+				}
+				if (c)
+				{
+					yield return Advance(arr3, ref k, ref c);
+				}
+			}
+		}
+
+		private static int Advance(IList<int> arr, ref int c, ref bool b)
+		{
+			c++;
+			b = c < arr.Count;
+			return arr[c - 1];
 		}
 	}
 }
