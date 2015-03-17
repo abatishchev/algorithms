7b569c1956456da1593b2064826e4d7d87fabd89
Alexander Batishchev <abatishchev@gmail.com>
2015-03-17 16:09:09 -0700

    Adding CutOffLinkedList

---
+++ Algorithms.Tests/CutOffLinkedListTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+	public class CutOffLinkedListTest : CutOffLinkedList
+	{
+		[Theory]
+		[MemberData("GetData")]
+		public void JumpLinkedList(LinkedNode<int> root, int cutHead, int expectedHead, int cutTail, int expectedTail)
+		{
+			LinkedNode<int> head = root, tail = null;
+
+			CutOff(ref head, ref tail, cutHead, cutTail);
+
+			head.Value.Should().Be(expectedHead);
+			tail.Value.Should().Be(expectedTail);
+		}
+
+		public static IEnumerable<object[]> GetData()
+		{
+			var root = new LinkedNode<int>(0);
+			Enumerable.Range(1, 10).Aggregate(root, (node, i) => node.Next = new LinkedNode<int>(i));
+			yield return new object[] { root, 3, 3, 3, 7 };
+		}
+	}
+}
---
+++ Algorithms/CutOffLinkedList.cs
@@ -0,0 +1,26 @@
+﻿namespace Algorithms
+{
+	public class CutOffLinkedList
+	{
+		public void CutOff(ref LinkedNode<int> head, ref LinkedNode<int> tail, int h, int t)
+		{
+			int i = -1;
+			LinkedNode<int> cur = head.Next;
+			tail = head;
+			while (cur != null)
+			{
+				i++;
+				if (i == t - 1)
+				{
+					head = cur;
+				}
+				if (i > t - 1)
+				{
+					tail = tail.Next;
+				}
+
+				cur = cur.Next;
+			}
+		}
+	}
+}
