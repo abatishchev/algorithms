b898acf22f1682d1571fe49d598f9830c9c66456
Alexander Batishchev <abatishchev@gmail.com>
2015-03-17 14:28:58 -0700

    Adding LeastRecentlyUsedCache

---
+++ Algorithms.Tests/LeastRecentlyUsedCacheTest.cs
@@ -0,0 +1,31 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+	public class LeastRecentlyUsedCacheTest
+	{
+		[Fact]
+		public void LeastRecentlyUsedCache()
+		{
+			var cache = new LeastRecentlyUsedCache<char>(3);
+
+			cache.Add('A');
+			cache.Add('B');
+			cache.Add('C');
+			cache.Add('D');
+
+			cache.Count.Should().Be(3);
+			cache.Contains('A').Should().BeFalse();
+			cache.Oldest.Should().Be('B');
+
+			cache.Remove('D');
+
+			cache.Count.Should().Be(2);
+			cache.Contains('D').Should().BeFalse();
+
+			var x = cache['B'];
+			cache.Newest.Should().Be(x);
+		}
+	}
+}
---
+++ Algorithms/LeastRecentlyUsedCache.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms
+{
+	public class LeastRecentlyUsedCache<T>
+	{
+		private readonly LinkedList<T> _list = new LinkedList<T>();
+
+		private readonly int _capacity;
+
+		private int _count;
+
+		public LeastRecentlyUsedCache(int capacity)
+		{
+			_capacity = capacity;
+		}
+
+		public void Add(T item)
+		{
+			if (_count == _capacity)
+			{
+				_list.RemoveFirst();
+			}
+
+			_list.AddLast(item);
+			_count++;
+
+		}
+
+		public void Remove(T item)
+		{
+			_list.Remove(item);
+		}
+
+		public T this[T item]
+		{
+			get
+			{
+				var node = _list.Find(item);
+				_list.Remove(node);
+				Add(item);
+				return item;
+			}
+		}
+
+		public bool Contains(T item)
+		{
+			return _list.Contains(item);
+		}
+
+		public int Count
+		{
+			get
+			{
+				return _list.Count;
+			}
+		}
+
+		public T Newest
+		{
+			get { return _list.Last.Value; }
+		}
+
+		public T Oldest
+		{
+			get { return _list.First.Value; }
+		}
+	}
+}
