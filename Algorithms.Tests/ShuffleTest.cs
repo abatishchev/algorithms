d8ef16189f2e1d5159abb91d10cb02c12dd28d6e
Alexander Batishchev <abatishchev@gmail.com>
2015-03-17 15:08:45 -0700

    Adding Shuffle

---
+++ Algorithms.Tests/ShuffleTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+	public class ShuffleTest
+	{
+		[Theory]
+		[MemberData("GetData")]
+		public void Shuffle(Type t, IList<int> values)
+		{
+			var sh = (IShuffle)Activator.CreateInstance(t);
+
+			values = sh.Do(values);
+
+			values.Should().NotBeAscendingInOrder()
+			      .And.Subject.Should().NotBeDescendingInOrder();
+		}
+
+		public static IEnumerable<object[]> GetData()
+		{
+			Func<int[]> gen = () => Enumerable.Range(0, 25).ToArray();
+
+			yield return new object[] { typeof(Shuffle1), gen() };
+			yield return new object[] { typeof(Shuffle2), gen() };
+		}
+	}
+}
---
+++ Algorithms/Shuffle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms
+{
+	public interface IShuffle
+	{
+		IList<int> Do(IList<int> values);
+	}
+
+	public class Shuffle1 : IShuffle
+	{
+		public IList<int> Do(IList<int> values)
+		{
+			var random = new Random();
+
+			values = values.OrderBy(_ => random.Next()).ToArray();
+
+			return values;
+		}
+	}
+
+	public class Shuffle2 : IShuffle
+	{
+		public IList<int> Do(IList<int> values)
+		{
+			var random = new Random();
+			for (int i = 1; i < values.Count; i++)
+			{
+				values.Swap(i, random.Next(0, i - 1));
+			}
+			return values;
+		}
+	}
+}
