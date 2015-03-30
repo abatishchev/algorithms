6891dc435067bdcc46e58409c0215300422232e3
Alexander Batishchev <abatishchev@gmail.com>
2015-03-30 12:34:13 -0700

    Adding SortOddAscEvenDesc

---
+++ Algorithms.Tests/SortOddAscEvenDescTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+	public class SortOddAscEvenDescTest
+	{
+		[Theory]
+		[MemberData("GetData")]
+		public void SortOddAscEvenDesc(Type t, IList<int> input, ICollection<int> expected)
+		{
+			var sort = (ISort)Activator.CreateInstance(t);
+
+			var actual = sort.Sort(input);
+
+			actual.Should().Equal(expected);
+		}
+
+		public static IEnumerable<object[]> GetData()
+		{
+			int[] input = { 1, 2, 3, 4, 5, 6 }, expected = { 1, 3, 5, 6, 4, 2 };
+
+			yield return new object[] { typeof(SortOddAscEvenDesc1), input, expected };
+			yield return new object[] { typeof(SortOddAscEvenDesc2), input, expected };
+		}
+	}
+}
---
+++ Algorithms/SortOddAscEvenDesc.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms
+{
+	public class SortOddAscEvenDesc1 : ISort
+	{
+		public IList<int> Sort(IList<int> input)
+		{
+			return input.OrderBy(i => i, new OddAscEvenDescComparer())
+					  .ToArray();
+		}
+	}
+
+	public class OddAscEvenDescComparer : IComparer<int>
+	{
+		public int Compare(int x, int y)
+		{
+			if (x == y)
+				return 0;
+
+			bool xe = IsEven(x), ye = IsEven(y);
+			if (xe && ye)
+			{
+				return x.CompareTo(y) * -1;
+			}
+			if (xe)
+			{
+				return 1;
+			}
+			if (ye)
+			{
+				return -1;
+			}
+			return x.CompareTo(y);
+		}
+
+		private static bool IsEven(int i)
+		{
+			return i % 2 == 0;
+		}
+	}
+}
