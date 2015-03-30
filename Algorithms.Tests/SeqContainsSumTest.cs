3e434cf20726a4b4c60a743fd65803e2841c5dd6
Alexander Batishchev <abatishchev@gmail.com>
2015-03-29 22:10:06 -0700

    Adding SeqContainsSum

---
+++ Algorithms.Tests/SeqContainsSumTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+	public class SeqContainsSumTest : SeqContainsSum
+	{
+		[Theory]
+		[MemberData("GetData")]
+		public void JumpLinkedList(ICollection<int> seq, int c, bool expected)
+		{
+			var actual = Contains(seq, c);
+
+			actual.Should().Be(expected);
+		}
+
+		public static IEnumerable<object[]> GetData()
+		{
+			yield return new object[] { new[] { 2, 3 }, 5, true };
+			yield return new object[] { new[] { 2, 3 }, 7, false };
+		}
+	}
+}
---
+++ Algorithms/SeqContainsSum.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms
+{
+	public class SeqContainsSum
+	{
+		public bool Contains(ICollection<int> seq, int c)
+		{
+			var set = new HashSet<int>(seq);
+
+			foreach (var i in seq)
+			{
+				if (set.Contains(c - i))
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+	}
+}
