67fd3d8de156f05b26a45cf7c78625e7bc3d60a7
Alexander Batishchev <abatishchev@gmail.com>
2015-04-02 17:06:20 -0700

    Adding MostFrequentElement

---
+++ Algorithms.Tests/MostFrequentElementTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+	public class MostFrequentElementTest
+	{
+		[Theory]
+		[MemberData("GetData")]
+		public void SortOddAscEvenDesc(Type t, ICollection<int> input, int expected)
+		{
+			var x = (IMostFrequentElement)Activator.CreateInstance(t);
+
+			var actual = x.FindMostFrequentElement(input);
+
+			actual.Should().Be(expected);
+		}
+
+		public static IEnumerable<object[]> GetData()
+		{
+			var input = new[] { 2, 3, 4, 5, 5, 3, 3 };
+			const int expected = 3;
+
+			yield return new object[] { typeof(MostFrequentElement1), input, expected };
+			yield return new object[] { typeof(MostFrequentElement1), input, expected };
+		}
+	}
+}
---
+++ Algorithms/MostFrequentElement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms
+{
+	public interface IMostFrequentElement
+	{
+		int FindMostFrequentElement(ICollection<int> input);
+	}
+
+	public class MostFrequentElement1 : IMostFrequentElement
+	{
+		public int FindMostFrequentElement(ICollection<int> input)
+		{
+			var dic = new Dictionary<int, int>();
+
+			int k = -1, max = 0;
+
+			foreach (var i in input)
+			{
+				int value;
+				if (dic.TryGetValue(i, out value))
+				{
+					value++;
+					dic[i] = value;
+
+					if (value > max)
+					{
+						max = value;
+						k = i;
+					}
+				}
+				else
+				{
+					dic.Add(i, 1);
+				}
+			}
+
+			return k;
+		}
+	}
+
+	public class MostFrequentElement2 : IMostFrequentElement
+	{
+		public int FindMostFrequentElement(ICollection<int> input)
+		{
+			return input.GroupBy(i => i)
+						.OrderBy(p => p.Count())
+						.Last().Key;
+		}
+	}
+}
