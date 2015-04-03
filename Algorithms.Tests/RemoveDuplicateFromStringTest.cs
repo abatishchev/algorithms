decaf19a95d1984d9a3a2166954de8accbd2fdf9
Alexander Batishchev <abatishchev@gmail.com>
2015-04-03 13:29:44 -0700

    Adding RemoveDuplicateFromString

---
+++ Algorithms.Tests/RemoveDuplicateFromStringTest.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+	public class RemoveDuplicateFromStringTest : RemoveDuplicateFromString
+	{
+		[Theory]
+		[InlineData("zeerroo", "zero")]
+		public void FindCommonElement2(string input, string expected)
+		{
+			string actual = Remove(input);
+
+			actual.Should().Be(expected);
+		}
+	}
+}
---
+++ Algorithms/RemoveDuplicateFromString.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms
+{
+	public class RemoveDuplicateFromString
+	{
+		public string Remove(string input)
+		{
+			var set = new HashSet<char>();
+			var sb = new StringBuilder();
+
+			foreach (var c in input)
+			{
+				if (!set.Contains(c))
+				{
+					set.Add(c);
+					sb.Append(c);
+				}
+			}
+			return sb.ToString();
+		}
+	}
+}
