a2333241393a16b540c57d95843a000c8d7761c6
Alexander Batishchev <abatishchev@gmail.com>
2015-04-03 12:08:41 -0700

    Adding FindCommonElement

---
+++ Algorithms.Tests/FindCommonElementTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+	public class FindCommonElementTest : FindCommonElement
+	{
+		[Theory]
+		[MemberData("GetData")]
+		public void FindCommonElement(int[] a, int[] b, int expected)
+		{
+			int actual = Find(a, b);
+
+			actual.Should().Be(expected);
+		}
+
+		public static IEnumerable<object[]> GetData()
+		{
+			yield return new object[]
+			{
+				new[] { 1, 2, 3, 4, 5 },
+				new[] { 6, 7, 8, 3, 9 },
+				3
+			};
+			yield return new object[]
+			{
+				new[] { 1, 2, 3, 4, 5 },
+				new[] { 6, 7, 8, 9, 0 },
+				-1
+			};
+		}
+	}
+}
---
+++ Algorithms/FindCommonElement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace Algorithms
+{
+	public class FindCommonElement
+	{
+		public int Find(int[] a, int[] b)
+		{
+			int[] greater, smaller;
+			if (a.Length <= b.Length)
+			{
+				greater = b;
+				smaller = a;
+			}
+			else
+			{
+				greater = a;
+				smaller = b;
+			}
+
+			var set = new HashSet<int>(greater);
+			foreach (var c in smaller)
+			{
+				if (set.Contains(c))
+					return c;
+			}
+			return -1;
+		}
+	}
+}
