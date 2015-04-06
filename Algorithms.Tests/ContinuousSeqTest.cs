239ca99bd62f1fa600362391e0cb34da86dcaf13
Alexander Batishchev <abatishchev@gmail.com>
2015-04-06 11:53:34 -0700

    Adding ContinuousSeq

---
+++ Algorithms.Tests/ContinuousSeqTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using FluentAssertions;
+using Xunit;
+
+namespace Algorithms.Tests
+{
+	public class ContinuousSeqTest : ContinuousSeq
+	{
+		[Theory]
+		[MemberData("GetData")]
+		public void ContinuousSeq(IEnumerable<int[]> seq, int[] expected)
+		{
+			int[] actual = GetContinuousSeq(seq);
+
+			actual.Should().BeEquivalentTo(expected);
+		}
+
+		public static IEnumerable<object[]> GetData()
+		{
+			yield return new object[]
+			{
+				new[]
+				{
+					new[] { 5, 4 }, new[] { 6, 2 }, new[] { 9, 3 }, new[] { 2, 5 }, new[] { 4, 9 }
+				},
+				new[] { 4, 9 }
+			};
+		}
+	}
+}
---
+++ Algorithms/ContinuousSeq.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace Algorithms
+{
+	public class ContinuousSeq
+	{
+		public int[] GetContinuousSeq(IEnumerable<int[]> seq)
+		{
+			int[] output = null;
+			int max = 0;
+
+			foreach (var s in seq)
+			{
+				if (IsSeq(s))
+				{
+					int size = GetSize(s);
+					if (max < size)
+					{
+						max = size;
+						output = s;
+					}
+				}
+			}
+
+			return output;
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static bool IsSeq(int[] seq)
+		{
+			return seq[0] < seq[1];
+		}
+
+		[MethodImpl(MethodImplOptions.AggressiveInlining)]
+		private static int GetSize(int[] seq)
+		{
+			return seq[1] - seq[0];
+		}
+	}
+}
