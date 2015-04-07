bec4c1533b2c81432f412afae7f3c3cca479c796
Alexander Batishchev <abatishchev@gmail.com>
2015-04-06 20:31:27 -0700

    Updating SumChildNodes

--- Algorithms.Tests/SumChildNodesTest.cs
+++ Algorithms.Tests/SumChildNodesTest.cs
@@ -1,49 +1,52 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using FluentAssertions;
 using Xunit;
 
 namespace Algorithms.Tests
 {
-	public class SumChildNodesTest : SumChildNodes
+	public class SumChildNodesTest
 	{
 		[Theory]
 		[MemberData("GetData")]
-		public void LinkListLooped(TreeNode<int> root, TreeNode<int> expected)
+		public void LinkListLooped(Type t, TreeNode<int> root, TreeNode<int> expected)
 		{
-			UpdateTreeNode(root);
+			dynamic x = Activator.CreateInstance(t);
+
+			x.UpdateTreeNode(root);
 
 			root.Should().Be(expected);
 		}
 
 		public static IEnumerable<object[]> GetData()
 		{
-			yield return new object[]
+			Func<TreeNode<int>> gen = () => new TreeNode<int>(5)
 			{
-				new TreeNode<int>(5)
+				Left = new TreeNode<int>(6)
 				{
-					Left = new TreeNode<int>(6)
-					{
-						Left = new TreeNode<int>(1),
-						Right = new TreeNode<int>(2)
-					},
-					Right = new TreeNode<int>(4)
-					{
-						Right = new TreeNode<int>(3)
-					}
+					Left = new TreeNode<int>(1),
+					Right = new TreeNode<int>(2)
 				},
-				new TreeNode<int>(21)
+				Right = new TreeNode<int>(4)
 				{
-					Left = new TreeNode<int>(9)
-					{
-						Left = new TreeNode<int>(1),
-						Right = new TreeNode<int>(2)
-					},
-					Right = new TreeNode<int>(7)
-					{
-						Right = new TreeNode<int>(3)
-					}
+					Right = new TreeNode<int>(3)
 				}
 			};
+			var expected = new TreeNode<int>(21)
+			{
+				Left = new TreeNode<int>(9)
+				{
+					Left = new TreeNode<int>(1),
+					Right = new TreeNode<int>(2)
+				},
+				Right = new TreeNode<int>(7)
+				{
+					Right = new TreeNode<int>(3)
+				}
+			};
+
+			yield return new object[] { typeof(SumChildNodes1), gen(), expected };
+			yield return new object[] { typeof(SumChildNodes2), gen(), expected };
 		}
 	}
 }
--- Algorithms/SumChildNodes.cs
+++ Algorithms/SumChildNodes.cs
@@ -1,6 +1,6 @@
 ﻿namespace Algorithms
 {
-	public class SumChildNodes
+	public class SumChildNodes1
 	{
 		public void UpdateTreeNode(TreeNode<int> node)
 		{
@@ -14,6 +14,20 @@
 				node.Value += node.Left.Value;
 			if (node.Right != null)
 				node.Value += node.Right.Value;
+		}
+	}
+
+	public class SumChildNodes2
+	{
+		public int UpdateTreeNode(TreeNode<int> node)
+		{
+			if (node == null)
+				return 0;
+
+			node.Value += UpdateTreeNode(node.Left);
+			node.Value += UpdateTreeNode(node.Right);
+
+			return node.Value;
 		}
 	}
 }
